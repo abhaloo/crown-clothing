3b2f8fb8b98e4b2aa57ce6f7fa4a1a576ce52981
Adeel <abhaloo@hotmail.com>
2020-06-14 20:06:52 -0600

    added a navbar component (called header) with links to home and shop page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact={true} path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
